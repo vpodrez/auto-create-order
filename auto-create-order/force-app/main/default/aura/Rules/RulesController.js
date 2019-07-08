3138c283516ac9996f7ec72b68f5b886920d0c5f
vyacheslav.podrez <vyacheslav.podrez@vrpconsulting.com>
2019-07-08 17:17:20 +0300

    Create controller and edit component

--- auto-create-order/force-app/main/default/aura/Rules/RulesController.js
+++ auto-create-order/force-app/main/default/aura/Rules/RulesController.js
@@ -1,5 +1,23 @@
 ({
-    myAction : function(component, event, helper) {
+  doInit: function(component, event, helper) {
+    var rules = [];
+    var action = component.get("c.getChooseRules");
+    var items = [];
+    action.setCallback(this, function(response) {
+      var state = response.getState();
+      if (state === "SUCCESS") {
+        rules = response.getReturnValue();
+        for (var i = 0; i < rules.length; i++) {
+          var item = {
+            label: rules[i],
+            value: rules[i]
+          };
+          items.push(item);
+        }
+        component.set("v.rules", items);
+      }
+    });
 
-    }
-})
+    $A.enqueueAction(action);
+  }
+});
